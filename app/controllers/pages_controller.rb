df0dce9be4a2d3fed8d4b42e8256240c3242fcf5
dnguye2 <dnguyen1093@gmail.com>
2020-05-18 15:49:38 -0700

    top ten lists of media added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
   def index
+    @top_albums = Work.top_ten_albums()
+    @top_books = Work.top_ten_books()
+    @top_movies = Work.top_ten_movies()
+    @spotlight = Work.spotlight()
   end
 end
