5877da5e1607a8235e732651d64b4c396105ad3e
dnguye2 <dnguyen1093@gmail.com>
2020-05-18 16:32:53 -0700

    new work functionality complete

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -6,6 +6,38 @@ class WorksController < ApplicationController
     @movies = Work.where(category: "movie")
   end
 
+  def show
+  end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+    @work = Work.create(work_params)
+    if @work.id?
+      redirect_to root_path
+    else
+      render :new
+    end
+
+    rescue ActionController::ParameterMissing
+      redirect_to new_work_path
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
 
 
+  private
+
+  def work_params
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+  end
 end
