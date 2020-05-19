34d9f3af4191d46c0b03789c7548cc86e3a60578
dnguye2 <dnguyen1093@gmail.com>
2020-05-19 12:44:15 -0700

    added custom routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+Rails.application.routes.draw do  
+  # Homepage
   root to: "pages#index"
   
+  # Works
   resources :works
 
+  # For Users
+  get "/login", to: "users#login_form", as: "login"
+  post "/login", to: "users#login"
+  post "/logout", to: "users#logout", as: "logout"
+  get "/users/current", to: "users#current", as: "current_user"
 end
