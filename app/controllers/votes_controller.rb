f80e46e2a4d14848045fe3a54f68d02ddd34151b
dnguye2 <dnguyen1093@gmail.com>
2020-05-20 10:53:45 -0700

    user can only upvote once

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,14 +2,26 @@ class VotesController < ApplicationController
   def create
     @current_user = User.find_by(id: session[:user_id])
     user_id = @current_user.id
+
+    
     vote = Vote.new(user_id: user_id, work_id: params[:work_id])
 
+    if Vote.where(user_id: @current_user.id, work_id: params[:work_id]).any?
+      flash[:error] = "You've already upvoted this work."
+      redirect_to works_path
+      return
+    end
+    
     if vote.save
       flash[:success] = "Succesfully upvoted!"
       redirect_to works_path
+      return
     else
       flash[:error] = "Could not upvote."
+      redirect_to works_path
+      return
     end
+
   end
 
 
