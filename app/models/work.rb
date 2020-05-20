c90a413f41612d47960d5e4bc4fa0c3cb14e42f3
dnguye2 <dnguyen1093@gmail.com>
2020-05-19 20:19:34 -0700

    updated spotlight method to incorporate vote total

--- app/models/work.rb
+++ app/models/work.rb
@@ -13,15 +13,43 @@ class Work < ApplicationRecord
 
   # Methods
   def self.spotlight
-    work = Work.all
+    # work = Work.all
 
-    spotlight_media = work.sample(1)
+    # spotlight_media = work.sample(1)
 
-    if !spotlight_media.nil?
-      return spotlight_media[0]
-    else
-      return nil
+    # if !spotlight_media.nil?
+    #   return spotlight_media[0]
+    # else
+    #   return nil
+    # end
+
+    works = Work.all
+
+    # spotlight_media = work.max_by(votes)
+
+    # if !spotlight_media.nil?
+    #   return spotlight_media
+    # else
+    #   return nil
+    # end
+
+    # each work
+    # key : work id
+    # value = work.votes.count
+
+    # use max_by to return id base off of highest value
+
+    # spotlight media = find by id
+
+    works_hash = {}
+
+    works.each do |work|
+      works_hash[work.id] = work.votes.count
     end
+
+    spotlight_id = works_hash.key(works_hash.values.max)
+    spotlight_media = Work.find_by(id: spotlight_id)
+
   end
 
   def self.top_ten(category)
