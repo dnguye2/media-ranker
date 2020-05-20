2a2e5ee27a4eeb0fb6bede925b947cd2cb3cf7ab
dnguye2 <dnguyen1093@gmail.com>
2020-05-19 19:53:04 -0700

    added validations

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,9 @@
 class Vote < ApplicationRecord
+  # Validations
+  validates :user_id, presence: true
+  validates :work_id, presence: true
+
+  # Relationships
   belongs_to :user
   belongs_to :work
 end
