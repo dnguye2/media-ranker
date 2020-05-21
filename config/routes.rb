5a623a689e2d62926a711cc9aa9fed96d86ca6bc
dnguye2 <dnguyen1093@gmail.com>
2020-05-21 14:47:34 -0700

    added user index route and removed vote index route

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,12 @@ Rails.application.routes.draw do
   resources :works
 
   # Users
-  resources :users, only: [:show]
+  resources :users, only: [:show, :index]
   get "/login", to: "users#login_form", as: "login"
   post "/login", to: "users#login"
   post "/logout", to: "users#logout", as: "logout"
   get "/users/current", to: "users#current", as: "current_user"
 
   # Votes
-  resources :votes, only: [:create, :index]
+  resources :votes, only: [:create]
 end
