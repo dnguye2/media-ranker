c206fa8f0706095ba34610a2c0284f9ff8c9994a
dnguye2 <dnguyen1093@gmail.com>
2020-05-20 23:41:20 -0700

    user instantiation tests made and pass

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 describe User do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  # Instantiation
+  let (:new_user) {
+    User.new(
+      username: "johannes_kelper"
+      )
+  }
+  
+  it "can be instantiated" do
+    expect(new_user.valid?).must_equal true
+  end
+
+  it "will have all required fields" do
+    new_user.save
+    user = User.first
+
+    [:username]. each do |field|
+      expect(user).must_respond_to field
+    end
+  end
 end
