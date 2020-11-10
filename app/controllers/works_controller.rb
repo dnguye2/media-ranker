153dc570a4cbbc22ba248856e7979973d52dc04b
MonaRahmani <monarahmanimehr@gmail.com>
2020-11-09 23:25:08 -0800

    works routes & controllers actions created

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,27 @@
 class WorksController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  private
+  def work_params
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+  end
 end
